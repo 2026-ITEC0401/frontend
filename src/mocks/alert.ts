72f3b63c63aa049b396a22a2f32dcfd8c4d92f77
zezeword <hanhui1823@gmail.com>
2026-08-19 03:18:29 +0900

    chore(ui): 알림 목업 데이터 추가

--- src/mocks/alert.ts
+++ src/mocks/alert.ts
@@ -7,14 +7,14 @@ export const mockAlertData: AlertWebData[] = [
     id: 1,
     time: "오전 08:00",
     location: "현관",
-    sound: "현관문 열림",
+    sound: "도어락",
     type: "Visitor", // 파랑 (방문자)
   },
   {
     id: 2,
     time: "오전 09:15",
-    location: "주방",
-    sound: "화재 경보기",
+    location: "거실",
+    sound: "화재",
     type: "Urgent", // 빨강 (긴급, 화재)
   },
   {
@@ -23,5 +23,12 @@ export const mockAlertData: AlertWebData[] = [
     location: "현관",
     sound: "아기 울음",
     type: "Noise", // 노랑 (생활 기기)
+  },
+  {
+    id: 4,
+    time: "오후 03:02",
+    location: "현관",
+    sound: "노크",
+    type: "Visitor", // 노랑 (생활 기기)
   },
 ];
