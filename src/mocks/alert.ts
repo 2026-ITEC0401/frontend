241139d5d2e6d990da3f7a564cd32bc9448c75bf
zezeword <hanhui1823@gmail.com>
2026-08-19 22:24:21 +0900

    style(ui): FullScreenAlert 디자인 시안 반영 및 하드코딩 스타일 정리

--- src/mocks/alert.ts
+++ src/mocks/alert.ts
@@ -7,7 +7,7 @@ export const mockAlertData: AlertWebData[] = [
     id: 1,
     time: "오전 08:00",
     location: "현관",
-    sound: "도어락",
+    sound: "방문",
     type: "Visitor", // 파랑 (방문자)
   },
   {
@@ -21,14 +21,14 @@ export const mockAlertData: AlertWebData[] = [
     id: 3,
     time: "오후 02:22",
     location: "현관",
-    sound: "아기 울음",
+    sound: "소음",
     type: "Noise", // 노랑 (생활 기기)
   },
   {
     id: 4,
     time: "오후 03:02",
     location: "현관",
-    sound: "노크",
+    sound: "방문",
     type: "Visitor", // 노랑 (생활 기기)
   },
 ];
